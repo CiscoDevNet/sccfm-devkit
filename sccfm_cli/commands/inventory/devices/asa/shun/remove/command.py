3f50a6c566302ca18a97e2595c1c09da21b8f8da
Andrei Huides <ahuides@cisco.com>
2026-03-20 18:45:34 +0200

    use share option for --wait

--- sccfm_cli/commands/inventory/devices/asa/shun/remove/command.py
+++ sccfm_cli/commands/inventory/devices/asa/shun/remove/command.py
@@ -10,9 +10,8 @@ from sccfm_cli.commands.inventory.devices.asa.shared import (
     AsaDeviceTargetCommand,
     asa_check_option,
     asa_device_filter_params,
-    asa_wait_option,
 )
-from sccfm_cli.commands.inventory.options import config_path_option, format_option
+from sccfm_cli.commands.inventory.options import config_path_option, format_option, wait_option
 from sccfm_cli.utils import with_spinner
 from sccfm_core.services.inventory.asa_shun_service import AsaShunService
 
@@ -49,7 +48,7 @@ class RemoveShunCommand(AsaDeviceTargetCommand):
                 ),
             ),
             asa_check_option(),
-            asa_wait_option(),
+            wait_option(),
             format_option(),
             config_path_option(),
         ]
